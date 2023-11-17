e0db2938d5678d50837e83f42151f9dc9b1e708a
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-11-17 14:54:59 +0100

    14:53

--- frontend/src/page/RootPage.js
+++ frontend/src/page/RootPage.js
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom"
 
+import MainNavigation from '../components/MainNavigation'
+
 const RootPage = () => {
     return (
         <>
-            <h1>STRONA GLOWNA</h1>
+            <MainNavigation />
             <main>
                 <Outlet />
             </main>
