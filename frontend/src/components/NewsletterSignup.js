6735c8cf6c96237c08e013a8cd9c925a6c895c0d
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-11-23 11:46:20 +0100

    tsdas

--- frontend/src/components/NewsletterSignup.js
+++ frontend/src/components/NewsletterSignup.js
@@ -1,24 +1,17 @@
 import { useEffect } from 'react';
-import { useFetcher } from 'react-router-dom';
-
 import classes from './NewsletterSignup.module.css';
+import {useFetcher } from 'react-router-dom';
 
 function NewsletterSignup() {
-  const fetcher = useFetcher();
-  const { data, state } = fetcher;
+    const fetcher = useFetcher()
 
-  useEffect(() => {
-    if (state === 'idle' && data && data.message) {
-      window.alert(data.message);
-    }
-  }, [data, state]);
+    const {data,state} = fetcher;
+
+   
+     
 
   return (
-    <fetcher.Form
-      method="post"
-      action="/newsletter"
-      className={classes.newsletter}
-    >
+    <fetcher.Form method="post" action='/newsletter' className={classes.newsletter}>
       <input
         type="email"
         placeholder="Sign up for newsletter..."
