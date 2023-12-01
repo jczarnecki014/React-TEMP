ae79af2650b961796f230d74674ae077860e1624
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-12-01 10:14:14 +0100

    tsdas

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ import EventDetails from './components/Events/EventDetails.jsx';
 import NewEvent from './components/Events/NewEvent.jsx';
 import EditEvent from './components/Events/EditEvent.jsx';
 
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './components/util/http.js';
+
 const router = createBrowserRouter([
   {
     path: '/',
@@ -38,7 +41,11 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />;
+    </QueryClientProvider>
+  )
 }
 
 export default App;
--- src/components/Events/FindEventSection.jsx
+++ src/components/Events/FindEventSection.jsx
@@ -1,11 +1,48 @@
 import { useRef } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
+
+import { fetchEvents } from '../util/http';
+
+import ErrorBlock from '../UI/ErrorBlock';
+import LoadingIndicator from '../UI/LoadingIndicator';
 
 export default function FindEventSection() {
   const searchElement = useRef();
 
+  const {data,isPending,isError,error} = useQuery({
+    queryKey: ['events',{searchTerm: searchElement}],
+    queryFn: ({signal}) => fetchEvents({signal,searchTerm:'test'})
+  })
+
   function handleSubmit(event) {
     event.preventDefault();
   }
+
+  let content = <p>Please enter a search term and to find events.</p>;
+
+  // if (isPending) {
+  //   content = <LoadingIndicator />;
+  // }
+
+  // if (isError) {
+  //   content = (
+  //     <ErrorBlock title="An error occurred" message="Failed to fetch events" />
+  //   );
+  // }
+
+  // if (data) {
+  //   content = (
+  //     <ul className="events-list">
+  //       {data.map((event) => (
+  //         <li key={event.id}>
+  //           <EventItem event={event} />
+  //         </li>
+  //       ))}
+  //     </ul>
+  //   );
+  // }
+
 
   return (
     <section className="content-section" id="all-events-section">
@@ -20,7 +57,7 @@ export default function FindEventSection() {
           <button>Search</button>
         </form>
       </header>
-      <p>Please enter a search term and to find events.</p>
+      {content}
     </section>
   );
 }
