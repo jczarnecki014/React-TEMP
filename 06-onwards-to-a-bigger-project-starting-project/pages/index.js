2503ab20d40de066faa774e1ef5a0d5d83ab1d19
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-12-06 12:16:42 +0100

    dsadasdas

---
+++ 06-onwards-to-a-bigger-project-starting-project/pages/index.js
@@ -0,0 +1,12 @@
+import MeetupList from '../components/meetups/MeetupList'
+
+
+const DUMMY_DATA = []
+
+const HomePage = () => {
+    return (
+        <MeetupList mee
+    )
+}
+
+export default HomePage;
