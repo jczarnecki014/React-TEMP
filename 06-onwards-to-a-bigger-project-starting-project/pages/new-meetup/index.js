c8820f18d39f4e7bc4cbe964b23f975bce986598
kubacz002 <106175428+kubacz002@users.noreply.github.com>
2024-01-09 06:20:05 +0100

    'Phase1'

---
+++ 06-onwards-to-a-bigger-project-starting-project/pages/new-meetup/index.js
@@ -0,0 +1,24 @@
+import MeetupList from '../../components/meetups/MeetupList'
+
+const DUMMY_DATA = [
+    {
+        id: 'm1',
+        title: 'A first Meetup',
+        image: 'https://prowly-uploads.s3.eu-west-1.amazonaws.com/uploads/landing_page/template_background/73142/12235d7b1498da10d0261ebd76a993a4.jpg',
+        address: 'Some adres 5, 12345 Some City',
+        description: 'This is a first meetup'
+    },
+    {
+        id: 'm2',
+        title: 'A second Meetup',
+        image: 'https://prowly-uploads.s3.eu-west-1.amazonaws.com/uploads/landing_page/template_background/73142/12235d7b1498da10d0261ebd76a993a4.jpg',
+        address: 'Some adres 5, 12345 Some City',
+        description: 'This is a second meetup'
+    },
+];
+
+const HomePage = () => {
+    return <MeetupList meetups={DUMMY_DATA} />
+}
+
+export default HomePage
