89986b15c67db033635822aaff6597244d8bd26f
student-wroclaw <student-wroclaw@EDU-WSB.local>
2024-01-09 08:55:02 +0100

    test

---
+++ 06-onwards-to-a-bigger-project-starting-project/pages/[meetupId]/index.js
@@ -0,0 +1,11 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail"
+
+const MeetupDetails = () => {
+    return <MeetupDetail 
+        image="https://prowly-uploads.s3.eu-west-1.amazonaws.com/uploads/landing_page/template_background/73142/12235d7b1498da10d0261ebd76a993a4.jpg" 
+        title="A first meetup" 
+        address="Some adres 5, 12345 Some City" 
+        description="This is a first meetup" />
+}
+
+export default MeetupDetails
--- 06-onwards-to-a-bigger-project-starting-project/pages/new-meetup/index.js
+++ 06-onwards-to-a-bigger-project-starting-project/pages/new-meetup/index.js
@@ -1,24 +1,12 @@
-import MeetupList from '../../components/meetups/MeetupList'
+import NewMeetupForm from '../../components/meetups/NewMeetupForm'
 
-const DUMMY_DATA = [
-    {
-        id: 'm1',
-        title: 'A first Meetup',
-        image: 'https://prowly-uploads.s3.eu-west-1.amazonaws.com/uploads/landing_page/template_background/73142/12235d7b1498da10d0261ebd76a993a4.jpg',
-        address: 'Some adres 5, 12345 Some City',
-        description: 'This is a first meetup'
-    },
-    {
-        id: 'm2',
-        title: 'A second Meetup',
-        image: 'https://prowly-uploads.s3.eu-west-1.amazonaws.com/uploads/landing_page/template_background/73142/12235d7b1498da10d0261ebd76a993a4.jpg',
-        address: 'Some adres 5, 12345 Some City',
-        description: 'This is a second meetup'
-    },
-];
+const NewMeetup = () => {
 
-const HomePage = () => {
-    return <MeetupList meetups={DUMMY_DATA} />
+    const AddMeetupHandler = (meetupData) => {
+        console.log(meetupData)
+    }
+
+    return <NewMeetupForm onAddMeetup={AddMeetupHandler} />
 }
 
-export default HomePage
+export default NewMeetup
