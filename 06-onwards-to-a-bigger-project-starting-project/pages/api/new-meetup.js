728fc65b15fbc80eb4d018af9664556402bf545b
kubacz002 <106175428+kubacz002@users.noreply.github.com>
2024-01-10 06:18:14 +0100

    'tdsad'

--- 06-onwards-to-a-bigger-project-starting-project/pages/api/new-meetup.js
+++ 06-onwards-to-a-bigger-project-starting-project/pages/api/new-meetup.js
@@ -3,7 +3,6 @@ import { MongoClient } from "mongodb";
 const handler = async (req,res) => {
     if(req.method === 'POST'){
         const data = req.body
-        const {title,image,address,description} = data
         
         const client = await MongoClient.connect('mongodb+srv://kubacz00:<Lukasz85>@cluster0.jn7jyug.mongodb.net/meetups?retryWrites=true&w=majority')
 
--- 06-onwards-to-a-bigger-project-starting-project/pages/new-meetup/index.js
+++ 06-onwards-to-a-bigger-project-starting-project/pages/new-meetup/index.js
@@ -1,9 +1,24 @@
 import NewMeetupForm from '../../components/meetups/NewMeetupForm'
 
+import {useRouter} from 'next/router'
+
 const NewMeetup = () => {
 
-    const AddMeetupHandler = (meetupData) => {
-        console.log(meetupData)
+    const router = useRouter();
+
+    const AddMeetupHandler = async (enteredMeetupData) => {
+        const result = await fetch('/api/new-meetups',{
+            method:'POST',
+            body: JSON.parse(enteredMeetupData),
+            headers:{
+                'Content-Type':'application/json'
+            }
+        });
+
+        console.log(result)
+
+        router.push('/')
+
     }
 
     return <NewMeetupForm onAddMeetup={AddMeetupHandler} />
