aff19ac0b76d946f9e87647042766bb568ce0f31
Jczarnecki014 <czarnecki.web@outlook.com>
2024-02-06 06:15:59 +0100

    'testy'

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,9 +1,15 @@
-
+import { useState } from "react"
 
 const Greeting = () => {
+
+    const [change,setchange] = useState(false)
+
     return (
         <div>
             <h1>Hello world!</h1>
+            <button onClick={()=>setchange(true)}>CLICK</button>
+            {!change && <p>Some paragraph is there</p> }
+            {change && <p>Change</p> }
         </div>
     )
 }
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,4 +1,6 @@
 import { render,screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
+
 import Greeting from "./Greeting"
 
 describe('Greeting component', () => {
@@ -12,5 +14,40 @@ describe('Greeting component', () => {
         //Assert
         const helloWorldElement = screen.getByText('Hello world!')
         expect(helloWorldElement).toBeInTheDocument();
+    })
+
+    test('renders "Some paragraph is there" when the BUTTON was not clicked',()=>{
+        //Arrange
+        render(<Greeting />)
+
+        //Act
+
+        //Assert
+        const paragraph = screen.getByText('Some paragraph is there',{exact:true})
+        expect(paragraph).toBeInTheDocument();
+    })
+
+    test('renders "CHANGED" when the BUTTON was clicked',()=>{
+        //Arrange
+        render(<Greeting />)
+
+        //Act
+        const button = screen.getByRole('button')
+        userEvent.click(button)
+
+        //Assert
+        const paragraph = screen.getByText('Change')
+        expect(paragraph).toBeInTheDocument();
+    })
+
+    test('does not to render "Some paragraph is there" when the BUTTON is clicked',() => {
+        //Arrange
+            render(<Greeting />)
+        //Act
+            const button = screen.getByRole('button')
+            userEvent.click(button)
+        //Assert
+            const paragraph = screen.queryByText('Some paragraph is there')
+            expect(paragraph).not.toBeInTheDocument()
     })
 })
